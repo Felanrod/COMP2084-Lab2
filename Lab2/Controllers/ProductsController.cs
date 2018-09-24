3a3a11c2490e34008d18d549de1cf1511a8e8faa
Joel Murphy <j.murphy2@mycanadore.ca>
2018-09-24 01:44:16 -0400

    only commit

---
+++ Lab2/Controllers/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab2.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Products/Details/Product-Name
+        public ActionResult Details(string ProductName)
+        {
+            ViewBag.Details = "You selected product: " + ProductName;
+            return View();
+        }
+    }
+}
